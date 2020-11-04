02c3249e63f6a0166e636cdcb8c919a298bceed2
qiaozhiguang <qiaozhiguang@qq.com>
2020-11-04 08:54:20 +0800

    角点检测 cornerHarri函数调整参数（内部为Sobel算子）

--- cornerHarris_Sobel.py
+++ cornerHarris_Sobel.py
@@ -4,7 +4,8 @@ import numpy as np
 img = cv2.imread('./meituan.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 gray = np.float32(gray)
-dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+# dst = cv2.cornerHarris(gray, 2, 3, 0.04) 
+dst = cv2.cornerHarris(gray, 2, 23, 0.04) 
 print(dst.max())
 print(dst)
 img[dst > 0.01 * dst.max()] = [0, 0, 255]
